a963bb78ba40084807ef2192c74752d6fd193af9
JP <jpalbert81@gmail.com>
2020-05-13 21:26:34 -0700

    update shell script

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SAVEFILE=$1
+PORT=$2
+
 echo 'Building..'
 docker build -t factorio-server .
 
@@ -11,7 +14,7 @@ echo 'Starting...'
 docker run --name factorio-server \
    -v /external/factorio/saves:/saves \
    -itd \
-   -p 34197:34197/udp \
+   -p $PORT:34197/udp \
    --restart always \
    factorio-server \
-   --start-server "/saves/jpaproject.zip"
+   --start-server $SAVEFILE
